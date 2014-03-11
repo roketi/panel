dac5d893d0e9ff974d1ef8950f51f6f1bccef176
Mario Rimann <mario@rimann.org>
2014-03-11 23:00:17 +0100

    [FEATURE] Add base for Behat tests

---
+++ Packages/Application/Roketi.Panel/Tests/Behavior/Features/Bootstrap/FeatureContext.php
@@ -0,0 +1,29 @@
+<?php
+
+use Behat\Behat\Exception\PendingException;
+use Behat\Gherkin\Node\TableNode,
+	Behat\MinkExtension\Context\MinkContext;
+use TYPO3\Flow\Utility\Arrays;
+use PHPUnit_Framework_Assert as Assert;
+
+require_once(__DIR__ . '/../../../../../Flowpack.Behat/Tests/Behat/FlowContext.php');
+
+/**
+ * Features context
+ */
+class FeatureContext extends MinkContext {
+	/**
+	 * @var \TYPO3\Flow\Object\ObjectManagerInterface
+	 */
+	protected $objectManager;
+
+	/**
+	 * Initializes the context
+	 *
+	 * @param array $parameters Context parameters (configured through behat.yml)
+	 */
+	public function __construct(array $parameters) {
+		$this->useContext('flow', new \Flowpack\Behat\Tests\Behat\FlowContext($parameters));
+		$this->objectManager = $this->getSubcontext('flow')->getObjectManager();
+	}
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+### Running the Behat Tests
+
+After installing Roketi Panel in your development environment, you need to run the following commands from the root of the installation to prepare your local Behat stuff::
+
+	./flow behat:kickstart
+
+This will install all the neccessary stuff in ``/Build/Behat/``. The Roketi.Panel Package comes with it's own features that can be tested/executed with the following command::
+
+	bin/behat -c Packages/Application/Roketi.Panel/Tests/Behavior/behat.yml
+
+Important: The default URL in the config is http://roketi-panel.dev/ - if your's differs, you need to changed that in the config file.
